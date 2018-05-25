6f39b2dc9ff5116ea0ec8352a00b87e42b5bf935
LinKinoMalandrin <armand.royneur@gmail.com>
2018-05-26 01:03:48 +0200

    index terminé

--- view/indexView.php
+++ view/indexView.php
@@ -18,7 +18,7 @@ ob_start();
 			Connection
 		</div>
 		<div id=Log-form>
-			<form action=post>
+			<form action=post action=index.php?action=auth>
 				<p>Adresse mail</p>
 				<input class=input type="mail" name="login" placeholder="ardresse@mail.xyz" required>
 				<p>Mot de passe</p>
@@ -31,14 +31,12 @@ ob_start();
 			Inscription
 		</div>
 		<div id=Reg-form>
-			<form action=post>
+			<form action=post action=index.php?action=addUser>
 				<p>Nom d'utilisateur (celui qui sera visible)</p>
-				<input class=input type="text" name="pwd" placeholder="Nom d'utilisateur" required>
+				<input class=input type="text" name="nom" placeholder="Nom d'utilisateur" required>
 				<p>Adresse mail</p>
 				<input class=input type="mail" name="login" placeholder="ardresse@mail.xyz" required>
 				<p>Mot de passe</p>
-				<input class=input type="password" name="pwd" placeholder="Mot de passe" required>
-				<p>Retapez votre mot de passe</p>
 				<input class=input type="password" name="pwd" placeholder="Mot de passe" required>
 				<input class="submit button" type="submit" value=Valider>
 			</form>
