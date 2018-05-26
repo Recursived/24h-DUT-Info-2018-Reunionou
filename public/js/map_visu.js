fc4ef2bfda59f642a1b5cb9cce53bad5beb20308
LinKinoMalandrin <armand.royneur@gmail.com>
2018-05-26 04:59:10 +0200

    rfgjetyj

--- public/js/map_visu.js
+++ public/js/map_visu.js
@@ -6,7 +6,7 @@
 		var input = coords.substring(1, coords.length - 1);
 		var latlngStr = input.split(',', 2);
         var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-        map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById('Map'), {
           center: latlng,
           zoom: 12
         });
--- view/eventView.php
+++ view/eventView.php
@@ -4,22 +4,17 @@ $title = 'Index';
 // Permet de creer la variable content
 ob_start();
 ?>
-
+	<script type="text/javascript" src="public/js/map_visu.js"></script>
+	    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-2fi4ap5nYpsfbI4YHUCLj4zZjfX0vEE&callback=initMap"
+    async defer></script>
 	<div id=Header>
+	<input type="hidden" id="coordsInput" value="(48.28064086716865, 4.871550789062439)">
 		<div id=Header-logo>
 			<img src="public/img/logo.png">
 		</div>
 		<h1>Evenement en cours</h1>
 		<div id=Unlog><a href=index.php?action=logout>Déconnexion</a></div>
 	</div>
-	
-<div id=Map>
-	Map
-</div>
-
-<div id=Response onclick="opening('From', 'Response')">
-	Je participe
-</div>
 
 <div id=View>
 	<div id=View-content>
@@ -50,6 +45,25 @@ ob_start();
 	</div>
 
 </div>
+
+<div id=Map>
+	Map
+</div>
+
+<div id=Response onclick="opening('Form', 'Response')">
+	Je participe
+</div>
+
+<div id=Form>
+	<div>
+		<form method=post action=index.php?action=participate>
+			<p>Votre commentaire</p>
+			<input class=input type="text" name="com">
+			<input type="submit" value="Valider">
+		</form>
+	</div>
+</div>
+
 
 <?php
 $content = ob_get_clean();
